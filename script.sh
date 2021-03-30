3d82f729d054c043edf12f73dfa97f87f5bbe161
vking34 <lzzvkingzzl@gmail.com>
2021-03-30 09:51:51 +0700

    fix(docs): engines

--- script.sh
+++ script.sh
@@ -4,18 +4,22 @@ mkdir -p ./docker-persistence/es/data
 sudo chmod 777 ./docker-persistence/es/data
 
 docker-compose up -d
-docker cp ./engines/engine1.json `docker ps -aqf "name=harness-cli"`:/data/engine1.json
+docker cp ./engines/engine.json `docker ps -aqf "name=harness-cli"`:/data/engine.json
 docker-compose exec harness-cli bash
 
 #
-hctl add /data/engine1.json
+hctl add /data/engine.json
 
 #
 docker-compose logs -f harness
-python ./samples/import_handmade.py --file ./samples/sample-handmade-data.txt
 
 #
-hctl train 1
+python ./samples/import_handmade.py --file ./samples/sample-handmade-data.txt
+
+python ./samples/import_handmade_cz.py --file ./samples/cz-sample-data.txt
+
+#
+hctl train 2
 
 
 #
